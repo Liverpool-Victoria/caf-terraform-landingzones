9131acfd63873da5010dc0e80db1ca266cb344dc
sunilkumar307 <sunil.kumar@lv.com>
2026-02-10 23:26:00 +0530

    Update source reference for landingzone module

--- caf_launchpad/landingzone.tf
+++ caf_launchpad/landingzone.tf
@@ -2,7 +2,7 @@ module "launchpad" {
   #source  = "aztfmod/caf/azurerm"
   #version = "5.7.8"
   # source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=main"
-  source  = "git::https://github.com/Liverpool-Victoria/terraform-azurerm-caf.git?ref=lvfs578"
+  source  = "git::https://github.com/Liverpool-Victoria/terraform-azurerm-caf.git?ref=AzDO-66716"
 
   providers = {
     azurerm.vhub = azurerm.vhub
